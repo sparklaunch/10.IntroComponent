6bc72b87ed81b4a770b2d40ba6f7cf48dd9fa4c9
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-21 10:02:42 +0900

    Add the try header

--- intro-component/src/components/Form.svelte
+++ intro-component/src/components/Form.svelte
@@ -1,7 +1,30 @@
-<div id="form" />
+<div id="form">
+    <div id="try">
+        <p>
+            <span class="emphasized">Try it free 7 days</span> then $20/mo. thereafter
+        </p>
+    </div>
+    <div id="trial-form" />
+</div>
 
 <style>
     #form {
         width: 50%;
+        display: flex;
+        flex-direction: column;
+    }
+    #try {
+        background-color: rgb(83, 74, 153);
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 10px rgba(0, 0, 0, 0.25);
+    }
+    #try > p {
+        text-align: center;
+        color: white;
+        font-weight: 400;
+    }
+    .emphasized {
+        font-weight: 600;
     }
 </style>
